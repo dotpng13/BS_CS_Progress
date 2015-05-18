a5ec09d7a1e6b43afcf76bf1881eebd5909028bf
Quinn Siebers <packerfan21396@gmail.com>
2015-05-18 17:12:02 -0500

    Added GUI
    Added JFrame and JPanel

---
+++ src/ProgramFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class ProgramFrame extends JFrame {
+	
+	public ProgramFrame() {}
+	public ProgramFrame(String title) {
+		this.setTitle(title);
+	}
+}
--- src/ProgressCalculator.java
+++ src/ProgressCalculator.java
@@ -1,9 +1,13 @@
-
 public class ProgressCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ProgramFrame frame = new ProgramFrame("BS & CS Progress");
+		ProgramPanel panel = new ProgramPanel();
+		frame.add(panel);
+		frame.setSize(800,450);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(ProgramFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 	}
 
 }
